338e9cda8ad1ac86944ac1e0817afe9a3747fc78
Daniel Mircea <daniel@viseztrance.com>
2021-01-25 00:13:15 +0200

    End of episode

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.order(created_at: :desc).paginate(page: params[:page], per_page: 2)
   end
 
   def show
